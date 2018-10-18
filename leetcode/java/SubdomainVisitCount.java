65986010981ae202cb564f94a118923bb0072b6a
gantushig <gantushig21@gmail.com>
2018-10-18 10:30:49 +0800

    2 problems done

---
+++ leetcode/java/SubdomainVisitCount.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class SubdomainVisitCount {
+    public List<String> subdomainVisits(String[] cpdomains) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
