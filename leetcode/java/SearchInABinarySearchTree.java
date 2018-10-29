586b4e327dbed519f120451d890fb1bf3cb4b5b3
gantushig <gantushig21@gmail.com>
2018-10-29 16:41:20 +0800

    3 problems are done

--- leetcode/java/SearchInABinarySearchTree.java
+++ leetcode/java/SearchInABinarySearchTree.java
@@ -1,26 +1,38 @@
-import java.util.*;
 
 public class SearchInABinarySearchTree {
     public TreeNode searchBST(TreeNode root, int val) {
-        if (root == null) return NULL;
+        if (root == null || root.val == val)
+            return root;
 
-        if (root.val == val) return root;
-
-        TreeNode left = searchBST(root.left, val);
-        TreeNode right = searchBST(root.right, val);
-
-        if (left != null) return left;
-        return right;
+        if (val < root.val)
+            return searchBST(root.left, val);
+        return searchBST(root.right, val);
     }
-    public static void main(String[] args) {
+
+    public void run() {
         TreeNode root = new TreeNode(4);
         root.left = new TreeNode(2);
         root.right = new TreeNode(7);
         root.left.left = new TreeNode(1);
         root.left.right = new TreeNode(3);
 
+        searchBST(root, 2);
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+
         SearchInABinarySearchTree problem = new SearchInABinarySearchTree();
 
-        System.out.println(problem.searchBST(root, 2).val);
+        problem.run();
     }
 }
