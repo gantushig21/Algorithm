5c330d7be1630f3cbdf409ece0cd56be7b117dee
gantushig <gantushig21@gmail.com>
2018-11-01 15:32:49 +0800

    Solving2

--- leetcode/java/MergeTwoBinaryTrees.java
+++ leetcode/java/MergeTwoBinaryTrees.java
@@ -2,8 +2,10 @@ import java.util.*;
 
 public class MergeTwoBinaryTrees {
     public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if (t1 == null) return t2;
-        if (t2 == null) return t1;
+        if (t1 == null)
+            return t2;
+        if (t2 == null)
+            return t1;
 
         t1.val += t2.val;
         t1.left = mergeTrees(t1.left, t2.left);
@@ -17,13 +19,15 @@ public class MergeTwoBinaryTrees {
         q.add(t);
         while (!q.isEmpty()) {
             TreeNode node = q.remove();
-            if (node.left != null) q.add(node.left);
-            if (node.right != null) q.add(node.right);
+            if (node.left != null)
+                q.add(node.left);
+            if (node.right != null)
+                q.add(node.right);
             System.out.println(node.val);
         }
     }
 
-    public static void main(String[] args) {
+    public void run() {
         TreeNode t1 = new TreeNode(1);
         t1.left = new TreeNode(3);
         t1.right = new TreeNode(2);
@@ -35,9 +39,22 @@ public class MergeTwoBinaryTrees {
         t2.left.right = new TreeNode(4);
         t2.right.right = new TreeNode(7);
 
-        MergeTwoBinaryTrees tree = new MergeTwoBinaryTrees();
-        
-        tree.printTree(tree.mergeTrees(t1, t2));
+        printTree(mergeTrees(t1, t2));
+    }
 
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        MergeTwoBinaryTrees problem = new MergeTwoBinaryTrees();
+
+        problem.run();
     }
 }
