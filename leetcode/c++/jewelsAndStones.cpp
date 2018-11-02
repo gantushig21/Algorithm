afee9da561ead6da45d1b5b803f44fe0deb1ed43
gantushig <gantushig21@gmail.com>
2018-11-02 14:45:26 +0800

    let s got c++

--- leetcode/c++/jewelsAndStones.cpp
+++ leetcode/c++/jewelsAndStones.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
+#include<string>
 
 using namespace std;
 
+class Solution {
+    public:
+        int numJewelsInStones(string J, string S) {
+            return 5;
+        }
+};
+
 int main() {
-    cout<<"Hello World"<<endl;
+    string J = "aA", S = "aAAbbbb";
+
+    Solution solution;
+    cout<<solution.numJewelsInStones(J, S)<<endl;
 }
