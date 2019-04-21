ac698bdacf2186bbe46540caf8c938aeb684ecc1
gantushigb <gantushig21@gmail.com>
2019-04-21 17:23:22 +0800

    LetterCasePermutation is done

--- leetcode/java/LetterCasePermutation.java
+++ leetcode/java/LetterCasePermutation.java
@@ -26,30 +26,59 @@ public class LetterCasePermutation {
     //     return list;
     // }
 
+    // public List<String> letterCasePermutation(String S) {
+    //     if (S == null)
+    //         return new LinkedList<>();
+
+    //     Queue<String> queue = new LinkedList<>();
+    //     queue.offer(S);
+
+    //     for (int i = 0; i < S.length(); i++) {
+    //         if (Character.isDigit(S.charAt(i))) continue;
+
+    //         int size = queue.size();
+    //         for (int j = 0; j < size; j++) {
+    //             char[] current = queue.poll().toCharArray();
+
+    //             current[i] = Character.toUpperCase(current[i]);
+    //             queue.offer(String.valueOf(current));
+
+    //             current[i] = Character.toLowerCase(current[i]);
+    //             queue.offer(String.valueOf(current));
+    //         }
+    //     }
+
+    //     return new LinkedList<>(queue);
+    // }
+
     public List<String> letterCasePermutation(String S) {
         if (S == null)
             return new LinkedList<>();
 
-        Queue<String> queue = new LinkedList<>();
-        queue.offer(S);
+        List<String> response = new LinkedList<>();
+        DFS(0, response, S.toCharArray());
 
-        for (int i = 0; i < S.length(); i++) {
-            if (Character.isDigit(S.charAt(i))) continue;
+        return response;
+    }
 
-            int size = queue.size();
-            for (int j = 0; j < size; j++) {
-                char[] current = queue.poll().toCharArray();
-
-                current[i] = Character.toUpperCase(current[i]);
-                queue.offer(String.valueOf(current));
-
-                current[i] = Character.toLowerCase(current[i]);
-                queue.offer(String.valueOf(current));
-            }
+    public void DFS(int index, List<String> response, char[] word) {
+        if (index == word.length) {
+            response.add(String.valueOf(word));
+            return;
         }
 
-        return new LinkedList<>(queue);
+        if (Character.isDigit(word[index])) {
+            DFS(index + 1, response, word);
+            return;
+        }
+        
+        word[index] = Character.toUpperCase(word[index]);
+        DFS(index + 1, response, word);
+
+        word[index] = Character.toLowerCase(word[index]);
+        DFS(index + 1, response, word);
     }
+
     public static void main(String[] args) {
         LetterCasePermutation problem = new LetterCasePermutation();
 
